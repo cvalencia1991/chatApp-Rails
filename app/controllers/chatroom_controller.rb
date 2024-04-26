75d5a3e91d6c4e7f254fe0a5a46d3090a8a98509
cvalencia1991 <cesar4a6z@gmail.com>
2024-04-26 08:46:55 -0500

    modify accesss for the message

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,5 +1,6 @@
 # Chartroom Controller
 class ChatroomController < ApplicationController
+  before_action :require_user
   def index
     @messages = Message.all
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_25_214533) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_26_014402) do
   create_table "messages", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
